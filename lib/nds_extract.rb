b216575f62b3e5861350f45e0295b7d7b96729c2
Ricardo Persaud <ricardo39985@gmail.com>
2019-11-09 23:24:35 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -44,7 +44,7 @@ def total_gross(source)
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
   total = 0
-  source.each do |key, value|
+  directors_totals(source).each do |key, value|
     total += value
   end
 
